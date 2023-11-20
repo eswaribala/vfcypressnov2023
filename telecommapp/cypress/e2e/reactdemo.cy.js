1964dc161197bbbeb2469e8cbb5d990c512d26fd
parameswari.bala <parameswaribala@gmail.com>
2023-11-20 17:09:11 +0530

    updated

--- telecommapp/cypress/e2e/reactdemo.cy.js
+++ telecommapp/cypress/e2e/reactdemo.cy.js
@@ -24,13 +24,14 @@ describe('kick start with cypress test',()=>{
         cy.log("Before invoked....")
         logs.push("Before invoked....")
         startServer()
-        cy.visit("http://localhost:3000")
+
 
     })
 
     beforeEach('Load the data',()=>{
         cy.log("Before Each invoked....")
         logs.push("Before Each invoked....")
+        cy.visit("http://localhost:3000")
         loadDataFromDb()
     })
     afterEach('close the db',()=>{
@@ -48,9 +49,20 @@ describe('kick start with cypress test',()=>{
 
     })
 
-    it('visit http://localhost:3000 and confirm it is running',()=>{
+    it('Test paragraph length to be 3 and\n' +
+        '            test 2nd paragraph contains postpaid',()=>{
 
         cy.log("Test Case 2 Called....")
+        cy.get('p')
+            .should('be.visible')
+            .should('have.length',3)
+
+        cy.get('p').should(($p)=>{
+            expect($p).to.have.length(3)
+        })
+        cy.get('p').should(($p)=>{
+            expect(($p).eq(1)).to.contain('Postpaid')
+        })
 
     })
     after('After All Test cases executed',()=>{
