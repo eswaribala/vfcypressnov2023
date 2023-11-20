8cc2512887b16a54f4704bcc2b909cf548406b3a
parameswari.bala <parameswaribala@gmail.com>
2023-11-20 15:18:43 +0530

    updated

--- telecommapp/cypress/e2e/reactdemo.cy.js
+++ telecommapp/cypress/e2e/reactdemo.cy.js
@@ -1,8 +1,12 @@
 describe('kick start with cypress test',()=>{
 
+    before('Visit the application',()=>{
+        cy.visit("http://localhost:3000")
+    })
+
     //visit http://localhost:3000 and confirm it is running
     it('visit http://localhost:3000 and confirm it is running',()=>{
-        cy.visit("http://localhost:3000")
+
         cy.screenshot()
     })
 
