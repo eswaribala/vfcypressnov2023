495ca638b54b90295354358d4925494b57be5f51
parameswari.bala <parameswaribala@gmail.com>
2023-11-20 15:25:09 +0530

    updated

--- telecommapp/cypress/e2e/reactdemo.cy.js
+++ telecommapp/cypress/e2e/reactdemo.cy.js
@@ -1,13 +1,41 @@
+function startServer(){
+
+}
+
+function stopServer(){
+
+}
+
+function  loadDataFromDb(){
+
+}
+
+function closeDB(){
+
+}
+
+
 describe('kick start with cypress test',()=>{
 
     before('Visit the application',()=>{
+        startServer()
         cy.visit("http://localhost:3000")
+
     })
 
+    beforeEach('Load the data',()=>{
+        loadDataFromDb()
+    })
     //visit http://localhost:3000 and confirm it is running
     it('visit http://localhost:3000 and confirm it is running',()=>{
 
         cy.screenshot()
     })
+    afterEach('close the db',()=>{
+        closeDB()
+    })
 
+    after('After All Test cases executed',()=>{
+        stopServer()
+    })
 })
