b359e88433d7c921614ff7982607e3d5623e4d0d
parameswari.bala <parameswaribala@gmail.com>
2023-11-20 15:35:42 +0530

    updated

--- telecommapp/cypress/e2e/reactdemo.cy.js
+++ telecommapp/cypress/e2e/reactdemo.cy.js
@@ -18,28 +18,33 @@ function closeDB(){
 describe('kick start with cypress test',()=>{
 
     before('Visit the application',()=>{
+        cy.log("Before invoked....")
         startServer()
         cy.visit("http://localhost:3000")
 
     })
 
     beforeEach('Load the data',()=>{
+        cy.log("Before Each invoked....")
         loadDataFromDb()
     })
     afterEach('close the db',()=>{
+        cy.log("After Each invoked....")
         closeDB()
     })
     //visit http://localhost:3000 and confirm it is running
     it('visit http://localhost:3000 and confirm it is running',()=>{
-
+        cy.log("Test Case 1 called....")
         cy.screenshot()
     })
 
     it('visit http://localhost:3000 and confirm it is running',()=>{
 
+        cy.log("Test Case 2 Called....")
         cy.screenshot()
     })
     after('After All Test cases executed',()=>{
+        cy.log("After invoked....")
         stopServer()
     })
 })
