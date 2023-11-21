d419fb506f0db8ba77c60c162a51d246587e6eee
parameswari.bala <parameswaribala@gmail.com>
2023-11-21 15:12:54 +0530

    updated

--- telecommapp/cypress/e2e/addcustomerjsonfixture.cy.js
+++ telecommapp/cypress/e2e/addcustomerjsonfixture.cy.js
@@ -1,15 +1,15 @@
 import "cypress-xpath"
 //step1
 describe('Telecom Project E2E Tests',()=>{
-
+    let testData
     Cypress.on('uncaught:exception', (err, runnable) => {
 // returning false here prevents Cypress from // failing the test
         return false
     });
     before('Load the json file',()=>{
         cy.fixture('customer').then(function (customer) {
-            cy.log(customer)
-            this.testdata = customer
+            cy.log(customer.firstName)
+            testData = customer
         })
     })
 
@@ -28,11 +28,11 @@ describe('Telecom Project E2E Tests',()=>{
 
         cy.get("#done").should('have.css','opacity','0')
             .click({force:true})
-        cy.get("input[id='fname']").should('be.visible').type(this.testdata.firstName)
-        cy.get("input[id='lname']").should('be.visible').type(this.testdata.lastName)
-        cy.get("input[id='email']").should('be.visible').type(this.testdata.email)
-        cy.get("textarea[id='message']").should('be.visible').type(this.testdata.address)
-        cy.get("input[id='telephoneno']").should('be.visible').type(this.testdata.mobileNo)
+        cy.get("input[id='fname']").should('be.visible').type(testData.firstName)
+        cy.get("input[id='lname']").should('be.visible').type(testData.lastName)
+        cy.get("input[id='email']").should('be.visible').type(testData.email)
+        cy.get("textarea[id='message']").should('be.visible').type(testData.address)
+        cy.get("input[id='telephoneno']").should('be.visible').type(testData.mobileNo)
         cy.get("#main > div > form > div > div:nth-child(9) > ul > li:nth-child(1) > input[type=submit]")
             .click({force:true})
         cy.xpath("//*[@id=\"main\"]/div/div/table/tbody/tr[1]/td[2]/h3")
