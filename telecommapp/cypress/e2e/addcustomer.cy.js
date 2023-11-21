f91cf6e2764798aef7f23f2273197b7954f802eb
parameswari.bala <parameswaribala@gmail.com>
2023-11-21 09:50:16 +0530

    updated

---
+++ telecommapp/cypress/e2e/addcustomer.cy.js
@@ -0,0 +1,16 @@
+//step1
+describe('Telecom Project E2E Tests',()=>{
+
+    beforeEach('visit telecom project',()=>{
+        cy.visit('https://demo.guru99.com/telecom/index.html')
+    })
+
+    it('Click on Add Customer',()=>{
+       cy.get("#one > div > div.flex-item.left > div:nth-child(1) > h3 > a")
+           .should('be.visible')
+           .click()
+    })
+
+
+})
+
