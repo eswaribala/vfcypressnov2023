470de18bec0f2cc7dbc8e4a8e0887a665114ac05
parameswari.bala <parameswaribala@gmail.com>
2023-11-21 16:39:59 +0530

    updated

--- telecommapp/cypress/e2e/addcustomerexcelfixture.cy.js
+++ telecommapp/cypress/e2e/addcustomerexcelfixture.cy.js
@@ -4,9 +4,25 @@ describe('Read Data from Excel File',()=>{
     before('Load and Parse Excel Sheet',()=>{
         cy.task(('parseXlsx',{ filePath: 'cypress/fixtures/customer.xlsx'}),(rows)=>{
                rowLength=rows.length;
-               cy.log(rowLength)
+
+            cy.writeFile("cypress/fixtures/xlsxData.json", { rows })
         })
 
     })
+
+    beforeEach('visit telecom project',()=>{
+        cy.visit('https://demo.guru99.com/telecom/index.html')
+    })
+
+    it('Click on Add Customer',()=>{
+        cy.get("#one > div > div.flex-item.left > div:nth-child(1) > h3 > a")
+            .should('be.visible')
+            .click()
+    })
+
+    it('Fill the customer Form',()=> {
+        cy.visit('https://demo.guru99.com/telecom/addcustomer.php')
+        cy.log(rowLength)
+    });
 
 })
