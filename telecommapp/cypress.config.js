7de43592a5002fbae3d569b5119f0e5ec53667f3
parameswari.bala <parameswaribala@gmail.com>
2023-11-21 16:25:21 +0530

    updated

--- telecommapp/cypress.config.js
+++ telecommapp/cypress.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require("cypress");
+const xlsx = require("node-xlsx").default;
+const fs = require("fs");
+const path = require("path");
 
 module.exports = defineConfig({
   e2e: {
@@ -9,7 +12,18 @@ module.exports = defineConfig({
     videosFolder:"./videos",
     setupNodeEvents(on, config) {
       // implement node event listeners here
-
+      on("task", {
+        parseXlsx({ filePath }) {
+          return new Promise((resolve, reject) => {
+            try {
+              const jsonData = xlsx.parse(fs.readFileSync(filePath));
+              resolve(jsonData);
+            } catch (e) {
+              reject(e);
+            }
+          });
+        }
+      });
     },
   },
 });
