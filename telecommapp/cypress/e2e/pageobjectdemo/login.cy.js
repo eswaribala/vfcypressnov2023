d11dc711b48897783e5eb12da68a65b230300c19
parameswari.bala <parameswaribala@gmail.com>
2023-11-22 14:37:01 +0530

    updated

---
+++ telecommapp/cypress/e2e/pageobjectdemo/login.cy.js
@@ -0,0 +1,28 @@
+import {BasePage} from "./BasePage";
+import {LoginPage} from "./LoginPage";
+import {SignUpPage} from "./SignUpPage";
+
+
+const basePage=new BasePage()
+const loginPage=new LoginPage()
+const signUpPage=new SignUpPage()
+
+describe('Signup and Login Insurance Project',()=>{
+    let testData
+    Cypress.on('uncaught:exception', (err, runnable) => {
+// returning false here prevents Cypress from // failing the test
+        return false
+    });
+    before('Load the json file',()=>{
+        cy.fixture('policyholder').then(function (policyholder) {
+
+            testData = policyholder
+        })
+    })
+
+    it('Launch the Insurance Project',()=>{
+        basePage.launchInsuranceProject()
+    })
+
+
+})
