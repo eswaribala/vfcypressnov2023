2fae7df1b51c015870483a1def4a295021f9fae4
parameswari.bala <parameswaribala@gmail.com>
2023-11-22 13:11:54 +0530

    updated

--- telecommapp/cypress/e2e/pageobjectdemo/BasePage.js
+++ telecommapp/cypress/e2e/pageobjectdemo/BasePage.js
@@ -2,5 +2,8 @@ export class BasePage{
     launchInsuranceProject(){
         cy.visit("https://demo.guru99.com/insurance/v1/index.php")
     }
+    launchRegistrationPage(){
+        cy.visit("https://demo.guru99.com/insurance/v1/register.php")
+    }
 
 }
